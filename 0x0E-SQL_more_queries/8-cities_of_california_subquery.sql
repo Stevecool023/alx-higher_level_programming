4190e7d9d951df3664170b4f24744583e3ae6050
STEPHEN <karanjastephenn@gmail.com>
2023-07-19 15:06:42 -0700

    MySQL

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,9 @@
--- list cities in a database.
-SELECT id, name
-FROM cities
-WHERE (SELECT id
-       FROM hbtn_0d_usa.states
-       WHERE name = "California"
-       )
-ORDER BY cities.id;
+-- Lists all cities of CA in the database hbtn_0d_usa.
+-- Results are ordered by ascending cities.id.
+SELECT `id`, `name`
+  FROM `cities`
+ WHERE `state_id` IN
+       (SELECT `id`
+	  FROM `states`
+	 WHERE `name` = "California")
+ ORDER BY `id`;
