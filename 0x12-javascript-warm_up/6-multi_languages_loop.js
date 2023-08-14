3d3152037051283f8b6cc188d4ad6105092f99ff
Stevecool023 <tallmankaranjastevee@gmail.com>
2023-08-14 16:00:34 -0700

    loops are fun

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,8 +3,8 @@
  a script that prints a multi-lines using an array of string and a loop.
  */
 
-const myLines = ['C is fun', 'Python is cool', 'JavaScript'];
+const arrayStrings = ['C is fun', 'Python is cool', 'JavaScript'];
 let i = 0;
-for (i; i < myLines.length; i++) {
-  console.log(myLines[i]);
+for (i; i < arrayStrings.length; i++) {
+  console.log(arrayStrings[i]);
 }
