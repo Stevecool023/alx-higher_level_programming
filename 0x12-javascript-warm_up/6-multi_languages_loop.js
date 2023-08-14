7e36be47e0a1fa3e5937daf17ecfd06d93fdf86c
Stevecool023 <tallmankaranjastevee@gmail.com>
2023-08-14 15:52:05 -0700

    loop to print multi-lines

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*
+ a script that prints a multi-lines
+ using an array of string and a loop.
+ */
+
+const myLines = ['C is fun', 'Python is cool', 'JavaScript'];
+let i = 0;
+for (i; i < myLines.length; i++) {
+  console.log(myLines[i]);
+}
