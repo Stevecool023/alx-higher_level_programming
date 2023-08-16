55224ace9ed0fa362d07b3cfab438aaafdd8bad0
Stevecool023 <tallmankaranjastevee@gmail.com>
2023-08-15 18:44:32 -0700

    class definition with constructor arguments

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// writing a class 'rectangle' that defines a rectangle.
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
